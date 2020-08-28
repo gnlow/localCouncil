dd6afdb1acf75b1fdda42db099e42a2a5e6a5c11
Gnlow <entrydark@gmail.com>
2020-08-29 00:10:46 +0900

    party correcting

--- wikiEdit.js
+++ wikiEdit.js
@@ -21,3 +21,46 @@ high.filter(x => x[1]).forEach(([district, name, party], i, l) => {
     }
 })
 const eHigh = high.filter(x => x[0])
+
+const names = {}
+Object.keys(data.member).forEach(x => {
+    names[x.split("_")[0]]?.push(x) || (names[x.split("_")[0]] = [x])
+})
+
+eHigh.slice(0).forEach(([district, name, party]) => {
+    if (names[name]) {
+        const target = names[name].filter(x=>data.member[x].district).length == 1 ? names[name].filter(x=>data.member[x].district)[0] :data.member[names[name].find(n => data.member[n].district == district.replace(" ", "").replace(/.*비례대표/, "비례대표"))]
+        if (target) {
+            target.party = party
+        } else {
+            console.log(
+`
+[warn] 동명이인 목록에서 일치하는 선거구가 없음.
+    ${district} ${name} / ${names[name].map(x => data.member[x].district).join(" | ")}`
+            )
+            const hardTarget = index => {
+                const newTarget = names[name][index]
+                newTarget.party = party
+                console.log(`    => resolved_HARDCODING ${data.member[newTarget].district}`)
+            }
+            // HARDCODING
+            switch (name) {
+                case "이재현":
+                    hardTarget(1)
+                    break
+            }
+        }
+    } else {
+        console.log(
+`
+[warn] 없는 이름.
+    ${district} ${name}`
+        )
+        // HARDCODING
+        switch (name) {
+            case "최미정":
+                console.log(`    => resolved_HARDCODING skip`)
+                break
+        }
+    }
+})
